f97692cd49f09f593b797490f511e53d6e9e2641
Tan Raksit <tan.raksit@bitkub.com>
2024-01-08 01:57:45 +0700

    feat: add room websocket - add join room function - add room info - edit port

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"strings"
+	"sync"
 	"time"
 
+	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/google/uuid"
@@ -21,7 +24,6 @@ type RoomRequest struct {
 	RoomName  string `json:"room_name"`
 	HostingID string `json:"hosting_id"`
 }
-
 type RoomResponse struct {
 	RoomID    string `json:"room_id"`
 	CreatedAt string `json:"created_at"`
@@ -30,8 +32,30 @@ type GuestSignInResponse struct {
 	UID string `json:"uuid"`
 }
 
+type User struct {
+	ID   string
+	Name string
+}
+type Room struct {
+	Name    string
+	Members []User
+}
+type MessageAction struct {
+	Action  string      `json:"action"`
+	Payload interface{} `json:"payload"`
+}
+
+type JoinRoomPayload struct {
+	Name string `json:"name"`
+}
+
+var (
+	clients   = make(map[*websocket.Conn]bool)
+	clientsMu sync.Mutex
+	rooms     = make(map[string]Room)
+)
+
 func generateUniqueRoomID() string {
-	// Combine random string, and UUID for better uniqueness
 	randomString := generateRandomString(5)
 	uuid := uuid.New().String()
 
@@ -49,8 +73,7 @@ func generateRandomString(length int) string {
 }
 
 func generateUUID() string {
-	// Combine random string, UUID and timestamp for better uniqueness
-	randomString := generateRandomString(5) // Generate a 5-character random string
+	randomString := generateRandomString(5)
 	uuid := uuid.New().String()
 	timestamp := time.Now().Format("20060102150405")
 
@@ -68,6 +91,7 @@ func createNewRoomHandler(c *fiber.Ctx) error {
 	}
 
 	roomID := generateUniqueRoomID()
+	rooms[roomID] = Room{Name: request.RoomName}
 
 	fmt.Printf("Room created: %s (%s)\n", request.RoomName, roomID)
 
@@ -88,11 +112,168 @@ func signInWithGuestHandler(c *fiber.Ctx) error {
 	})
 }
 
+func broadcastMessage(roomId string, message any) {
+	clientsMu.Lock()
+	defer clientsMu.Unlock()
+
+	for client := range clients {
+		cRoomId := client.Locals("roomId")
+		if cRoomId != roomId {
+			continue
+		}
+
+		err := client.WriteJSON(message)
+
+		if err != nil {
+			log.Printf("Error sending message to client: %v", err)
+		}
+	}
+}
+
+func broadcastRoomInfo(roomId string) {
+	broadcastMessage(roomId, rooms[roomId])
+}
+
+func roomExists(roomId string) bool {
+	room, exists := rooms[roomId]
+	log.Println(room)
+	return exists
+}
+
+func foundUser(uid string, members []User) bool {
+	found := false
+	for _, user := range members {
+		if user.ID == uid {
+			found = true
+			break
+		}
+	}
+	return found
+}
+func joinRoom(payload interface{}, uid string, room *Room) bool {
+	joinRoomPayload, ok := payload.(map[string]interface{})
+	if !ok {
+		fmt.Println("Invalid payload format for REGISTER action.")
+		return false
+	}
+
+	var joinRoomData JoinRoomPayload
+	payloadBytes, err := json.Marshal(joinRoomPayload)
+	if err != nil {
+		fmt.Println("Error marshaling payload:", err)
+		return false
+	}
+
+	err = json.Unmarshal(payloadBytes, &joinRoomData)
+	if err != nil {
+		fmt.Println("Error unmarshaling payload:", err)
+		return false
+	}
+
+	name := joinRoomData.Name
+	room.Members = append(room.Members, User{ID: uid, Name: name})
+
+	return true
+}
+
+func leaveRoom(uid string, roomId string) bool {
+	var filteredMembers []User
+	for _, user := range rooms[roomId].Members {
+		if user.ID != uid {
+			filteredMembers = append(filteredMembers, user)
+		}
+	}
+
+	room := rooms[roomId]
+	room.Members = filteredMembers
+	rooms[roomId] = room
+
+	log.Printf("(%s) leaved room(%s) %s", uid, roomId, filteredMembers)
+	return true
+}
+
+func handleRoomSocket(c *websocket.Conn) {
+	roomId := c.Params("id")
+
+	if !roomExists(roomId) {
+		c.WriteJSON("Room not found")
+		log.Printf("Room with ID %s not found", roomId)
+		c.Close()
+		return
+	}
+	uid := c.Params("uid")
+
+	c.Locals("roomId", roomId)
+	c.WriteJSON(MessageAction{Action: "UPDATE_ROOM", Payload: rooms[roomId]})
+	clientsMu.Lock()
+	clients[c] = true
+	clientsMu.Unlock()
+
+	defer func() {
+		clientsMu.Lock()
+		delete(clients, c)
+		clientsMu.Unlock()
+		if foundUser(uid, rooms[roomId].Members) {
+			leaveRoom(uid, roomId)
+		} else {
+			log.Printf("user unregistered leaved room(%s)", roomId)
+		}
+		_ = c.Close()
+	}()
+
+	if !foundUser(uid, rooms[roomId].Members) {
+		c.WriteJSON(fiber.Map{"action": "NEED_TO_JOIN"})
+	}
+
+	var (
+		msg []byte
+		err error
+	)
+	for {
+		if _, msg, err = c.ReadMessage(); err != nil {
+			log.Println("read:", err)
+			break
+		}
+
+		log.Printf("recv: %s", msg)
+		var receivedMessage MessageAction
+		if err := json.Unmarshal(msg, &receivedMessage); err != nil {
+			log.Println("json unmarshal:", err)
+			break
+		}
+
+		switch receivedMessage.Action {
+		case "JOIN_ROOM":
+			room := rooms[roomId]
+
+			if !foundUser(uid, rooms[roomId].Members) && joinRoom(receivedMessage.Payload, uid, &room) {
+				rooms[roomId] = room
+				c.WriteJSON(room)
+
+				broadcastMessage(roomId, MessageAction{Action: "UPDATE_ROOM", Payload: room})
+			} else {
+				c.WriteJSON(fiber.Map{"error": "JOIN_ROOM_FAILED"})
+			}
+		}
+
+	}
+}
+
 func main() {
 	app := fiber.New()
 	app.Use(cors.New())
 	app.Get("/health", healthCheckHandler)
 	api := app.Group("/api")
+
+	app.Use("/ws", func(c *fiber.Ctx) error {
+		if websocket.IsWebSocketUpgrade(c) {
+			c.Locals("allowed", true)
+			return c.Next()
+		}
+		return fiber.ErrUpgradeRequired
+	})
+
+	app.Get("/ws/room/:uid/:id", websocket.New(handleRoomSocket))
 
 	v1 := api.Group("v1")
 	v1.Get("/", func(c *fiber.Ctx) error {
@@ -101,5 +282,5 @@ func main() {
 	v1.Get("/guest/sign-in", signInWithGuestHandler)
 	v1.Post("/new-room", createNewRoomHandler)
 
-	app.Listen(":8000")
+	app.Listen(":3001")
 }
